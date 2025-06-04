da27757c3021243b4677104e960a6a5b44886ae5
Hong <97hongjun@gmail.com>
2025-06-04 20:51:45 +0000

    add code for joint log loss and sgd variants.

--- neural_testbed.py
+++ neural_testbed.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from model import MLP
+from model import MLP, Epinet
 from torch import Tensor
 from data import FastTensorDataLoader
 import os
@@ -70,13 +70,13 @@ def train(model: nn.Module, train_loader: FastTensorDataLoader, test_loader: Fas
     return model
             
 def train_single_pass_logistic(model: nn.Module, train_loader: FastTensorDataLoader, optimizer: torch.optim.Optimizer,
-                               device: str, filename: str, log_freq: int, swa_model: Optional[AveragedModel]=None, counter: int=0) -> tuple[nn.Module, int]:
+                               device: str, filename: str, log_freq: int, swa_model: Optional[AveragedModel]=None, counter: int=0, total_loss: float=0.0, total_swa_loss: float=0.0) -> tuple[nn.Module, int, float, float]:
     model.train()
     log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
     
     total_samples = counter
-    total_loss = 0.0
-    total_swa_loss = 0.0
+    total_loss = total_loss
+    total_swa_loss = total_swa_loss
     for i, (x, y, probs) in enumerate(train_loader):
         x, y, probs = x.to(device), y.to(device), probs.to(device)
         pred = nn.Sigmoid()(model(x))
@@ -86,7 +86,11 @@ def train_single_pass_logistic(model: nn.Module, train_loader: FastTensorDataLoa
         true_prob = probs[torch.arange(len(probs)), y]
         loss = torch.sum(torch.log(true_prob) - torch.log(pred_prob))
         total_samples += len(x)
-        total_loss += loss.item()
+
+        true_kl = torch.log(true_prob) - torch.log(pred_prob)
+        true_kl2 = torch.log(1.0 - true_prob) - torch.log(1.0 - pred_prob)
+        true_kl_full = true_prob * true_kl + (1.0 - true_prob) * true_kl2
+        total_loss += torch.sum(true_kl_full).item()
 
         if swa_model is not None:
             swa_pred = nn.Sigmoid()(swa_model(x))
@@ -94,7 +98,9 @@ def train_single_pass_logistic(model: nn.Module, train_loader: FastTensorDataLoa
             swa_pred_prob = swa_pred[torch.arange(len(swa_pred)), y]
             swa_true_prob = probs[torch.arange(len(probs)), y]
             swa_kl = torch.log(swa_true_prob) - torch.log(swa_pred_prob)
-            swa_loss = torch.sum(swa_kl)
+            swa_kl2 = torch.log(1.0 - swa_true_prob) - torch.log(1.0 - swa_pred_prob)
+            swa_kl_full = swa_true_prob * swa_kl + (1.0 - swa_true_prob) * swa_kl2
+            swa_loss = torch.sum(swa_kl_full)
             total_swa_loss += swa_loss.item()
         
         optimizer.zero_grad()
@@ -105,7 +111,7 @@ def train_single_pass_logistic(model: nn.Module, train_loader: FastTensorDataLoa
             swa_model.update_parameters(model)
             # swa_scheduler.step()
         if i % log_freq == 0:
-            if total_samples == 0:
+            if total_samples == len(x):
                 with open(log_file, 'w') as f:
                     if swa_model is not None:   
                         f.write("batch,loss,swa_loss\n")
@@ -117,18 +123,19 @@ def train_single_pass_logistic(model: nn.Module, train_loader: FastTensorDataLoa
                         f.write(f"{total_samples},{total_loss/total_samples},{total_swa_loss/total_samples}\n")
                     else:
                         f.write(f"{total_samples},{total_loss/total_samples}\n")
-    return model, total_samples
+    return model, total_samples, total_loss, total_swa_loss
 
 
 
 def train_single_pass(model: nn.Module, train_loader: FastTensorDataLoader, optimizer: torch.optim.Optimizer, 
-device: str, filename: str, log_freq: int, swa_model: Optional[AveragedModel]=None, swa_scheduler: Optional[SWALR]=None) -> nn.Module:
+                      device: str, filename: str, log_freq: int, swa_model: Optional[AveragedModel]=None, counter: int=0,
+                      total_loss: float=0.0, total_swa_loss: float=0.0, lr_scheduler: Optional[torch.optim.lr_scheduler._LRScheduler]=None) -> tuple[nn.Module, int, float, float]:
     model.train()
     log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
     
-    total_samples = 0
-    total_loss = 0.0
-    total_swa_loss = 0.0
+    total_samples = counter
+    total_loss = total_loss
+    total_swa_loss = total_swa_loss
     for i, (x, y, probs) in enumerate(train_loader):
         x, y, probs = x.to(device), y.to(device), probs.to(device)
         pred = nn.Softmax(dim=-1)(model(x))
@@ -136,27 +143,30 @@ device: str, filename: str, log_freq: int, swa_model: Optional[AveragedModel]=No
         pred_prob = pred[torch.arange(len(pred)), y]
         true_prob = probs[torch.arange(len(probs)), y]
         kl = torch.log(true_prob) - torch.log(pred_prob)
+
         loss = torch.sum(kl)
         total_samples += len(x)
-        total_loss += loss.item()
 
+        true_kl = torch.log(probs) - torch.log(pred)
+        true_kl_full = probs * true_kl
+        total_loss += torch.sum(true_kl_full).item()
+        
         if swa_model is not None:
             swa_pred = nn.Softmax(dim=-1)(swa_model(x))
-            swa_pred_prob = swa_pred[torch.arange(len(swa_pred)), y]
-            swa_true_prob = probs[torch.arange(len(probs)), y]
-            swa_kl = torch.log(swa_true_prob) - torch.log(swa_pred_prob)
-            swa_loss = torch.sum(swa_kl)
-            total_swa_loss += swa_loss.item()
+            true_kl_swa = torch.log(probs) - torch.log(swa_pred)
+            true_kl_full_swa = probs * true_kl_swa
+            total_swa_loss += torch.sum(true_kl_full_swa).item()
         
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        if lr_scheduler is not None:
+            lr_scheduler.step()
 
-        if swa_model is not None and swa_scheduler is not None:
+        if swa_model is not None:
             swa_model.update_parameters(model)
-            # swa_scheduler.step()
         if i % log_freq == 0:
-            if i == 0:
+            if total_samples == len(x):
                 with open(log_file, 'w') as f:
                     if swa_model is not None:   
                         f.write("batch,loss,swa_loss\n")
@@ -168,7 +178,31 @@ device: str, filename: str, log_freq: int, swa_model: Optional[AveragedModel]=No
                         f.write(f"{total_samples},{total_loss/total_samples},{total_swa_loss/total_samples}\n")
                     else:
                         f.write(f"{total_samples},{total_loss/total_samples}\n")
-    return model
+    return model, total_samples, total_loss, total_swa_loss
+
+def evaluate_joint_logloss(model: Epinet, x: torch.Tensor, y: torch.Tensor, probs: torch.Tensor, num_indices: int) -> float:
+    model.eval()
+    with torch.no_grad():
+        total_kl = 0.0
+        num_samples = 0
+        z = torch.randn(num_indices, model.index_dim, generator=model.generator)
+        pred = nn.Softmax(dim=-1)(model(x, z))
+        # shape of (batch_size * num_indices, output_dim)
+        pred = pred.view(batch_size, num_indices, output_dim)
+        # shape of (batch_size, num_indices,)
+        pred_terms = pred[torch.arange(len(pred)),:, y]
+
+        # shape of (num_indices,)
+        joint_preds = torch.product(pred_terms, dim=0)
+        joint_pred = torch.mean(joint_preds, dim=0)
+
+        # of shape (output_dim)
+        prob_terms = probs[torch.arange(len(probs)),y]
+        kl_term = torch.sum(torch.log(prob_terms), dim=0) - torch.log(joint_pred)
+
+        total_kl += kl_term.item()
+        num_samples += len(x)
+    return total_kl / float(num_samples)
 
 def evaluate(model: nn.Module, test_loader: FastTensorDataLoader, device: str) -> float:
     model.eval()
@@ -188,38 +222,54 @@ def evaluate(model: nn.Module, test_loader: FastTensorDataLoader, device: str) -
     return total_kl / float(num_samples)
 
 if __name__ == "__main__":
-    input_dim = 10
-    hidden_dims_model = [64, 64]
+    input_dim = 64
+    hidden_dims_model = [128, 128]
     output_dims = 2
-    hidden_dims_agent = [256, 256]
+    hidden_dims_agent = [2048, 2048]
     swa = True
     if torch.cuda.is_available():
         device = "cuda"
     else:
         device = "cpu"
-    data_generator = torch.Generator(device="cpu").manual_seed(42)
-    data_gpu_generator = torch.Generator(device="cuda").manual_seed(42)
-    agent_generator = torch.Generator(device="cpu").manual_seed(43)
-    agent_gpu_generator = torch.Generator(device="cuda").manual_seed(43)
+    optim = 'Adam'
+    data_seed = 50
+    agent_seed = 51
+    data_generator = torch.Generator(device="cpu").manual_seed(data_seed)
+    data_gpu_generator = torch.Generator(device="cuda").manual_seed(data_seed)
+    agent_generator = torch.Generator(device="cpu").manual_seed(agent_seed)
+    agent_gpu_generator = torch.Generator(device="cuda").manual_seed(agent_seed)
     model = MLP(input_dim, hidden_dims_model, output_dims, generator=data_gpu_generator).to(device)
     agent = MLP(input_dim, hidden_dims_agent, output_dims, generator=agent_gpu_generator).to(device)
     model.apply(model.init_xavier_uniform)
     agent.apply(agent.init_xavier_uniform)
-    optimizer = torch.optim.Adam(agent.parameters(), lr=0.001)
+    if optim == 'SGD':
+        optimizer = torch.optim.SGD(agent.parameters(), lr=0.001)
+        lambda1 = lambda x: 1.0/(x + 1.0)
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda1)
+    elif optim == 'Adam':
+        optimizer = torch.optim.Adam(agent.parameters(), lr=0.001)
     if swa:
-        swa_model = AveragedModel(agent, avg_fn=torch.optim.swa_utils.get_ema_multi_avg_fn(0.9))
-        swa_scheduler = SWALR(optimizer, swa_lr=0.05)
-        filename = f"logs/training_loss_in{input_dim}_hid{hidden_dims_model[0]}_out{output_dims}_hid{hidden_dims_agent[0]}_swa2.log"
+        swa_model = AveragedModel(agent)
+        if optim == 'SGD':
+            filename = f"logs/sgd/training_loss_in{input_dim}_hid{hidden_dims_model[0]}_out{output_dims}_hid{hidden_dims_agent[0]}_data_seed{data_seed}_agent_seed{agent_seed}_swa.log"
+        else:
+            filename = f"logs/adam/training_loss_in{input_dim}_hid{hidden_dims_model[0]}_out{output_dims}_hid{hidden_dims_agent[0]}_data_seed{data_seed}_agent_seed{agent_seed}_swa.log"
     else:
-        filename = f"logs/training_loss_in{input_dim}_hid{hidden_dims_model[0]}_out{output_dims}_hid{hidden_dims_agent[0]}.log"
-    X_train, Y_train, probs_train = generateSample(input_dim, model, 2**24, data_generator, device)  
-    train_loader = get_dataloaders(X_train, Y_train, probs_train, batch_size=128, shuffle=True, keep_last=True, generator=agent_generator, device=device)
-    # X_test, Y_test, probs_test = generateSample(input_dim, model, 2**16, data_generator, device)
-    # test_loader = get_dataloaders(X_test, Y_test, probs_test, batch_size=128, shuffle=False, keep_last=True, generator=agent_generator, device=device)
-    # agent = train(agent, train_loader, test_loader, num_epochs=100, optimizer=optimizer, device=device, filename=filename)
-    if swa:
-        agent = train_single_pass(agent, train_loader, optimizer, device, filename, log_freq=2**10, swa_model=swa_model, swa_scheduler=swa_scheduler)
-    else:
-        agent = train_single_pass(agent, train_loader, optimizer, device, filename, log_freq=2**10)
-    # kl = evaluate(agent, test_loader, device=device)
-    # print(f"KL: {kl}")
+        if optim == 'SGD':
+            filename = f"logs/sgd/training_loss_in{input_dim}_hid{hidden_dims_model[0]}_out{output_dims}_hid{hidden_dims_agent[0]}_data_seed{data_seed}_agent_seed{agent_seed}.log"
+        else:
+            filename = f"logs/adam/training_loss_in{input_dim}_hid{hidden_dims_model[0]}_out{output_dims}_hid{hidden_dims_agent[0]}_data_seed{data_seed}_agent_seed{agent_seed}.log"
+    train_samples = 30
+    total_loss = 0.0
+    total_swa_loss = 0.0
+    counter = 0
+    for _ in range(2**train_samples//2**16):
+        print(f"Training {counter} samples")
+        X_train, Y_train, probs_train = generateSample(input_dim, model, 2**16, data_generator, device)  
+        train_loader = get_dataloaders(X_train, Y_train, probs_train, batch_size=128, shuffle=True, keep_last=True, generator=agent_generator, device=device)
+        if swa:
+            agent, counter, total_loss, total_swa_loss = train_single_pass(agent, train_loader, optimizer, device, filename, log_freq=2**10, swa_model=swa_model, counter=counter, total_loss=total_loss, total_swa_loss=total_swa_loss)
+        else:
+            agent, counter, total_loss, _ = train_single_pass(agent, train_loader, optimizer, device, filename, log_freq=2**10, counter=counter, total_loss=total_loss)
+        del X_train, Y_train, probs_train, train_loader
+        torch.cuda.empty_cache()
